8eef9b836228dbcb8edec81994272de8106f132b
Ludovic Vigouroux <ludo@mundoludo.fr>
2021-11-22 11:49:44 +0100

    Add copypaste shortcuts for urxvt + various fixes

--- gnu-linux/scripts/update-gentoo.sh
+++ gnu-linux/scripts/update-gentoo.sh
@@ -1,4 +1,5 @@
 emerge-webrsync
-emerge --update --deep --with-bdeps=y --newuse --quiet --keep-going @world
+layman -S
+emerge --update --deep --with-bdeps=y --newuse --quiet-build --keep-going @world
 emerge --depclean
 revdep-rebuild
